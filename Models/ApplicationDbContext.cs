e2e258851a83c9d086a82b6d0c4c2b7441b8fc5f
Михаил Овчинников <swedisch97@gmail.com>
2019-12-10 21:54:06 +0300

    Package to use Entity FrameWork is "dotnet add package Microsoft.EntityFrameworkCore.Sqlite dotnet add package Microsoft.EntityFrameworkCore.Design"

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions)
+    }
+}
