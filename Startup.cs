ce86579b46f9b16f9ab4d3f16ab6a197ac16eb88
Михаил Овчинников <swedisch97@gmail.com>
2019-12-10 18:09:53 +0300

    Ready to begin

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +25,12 @@ namespace SportsStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMvc(routes=> { });
+                app.UseStaticFiles();
+                app.UseStatusCodePages();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            
         }
     }
 }
